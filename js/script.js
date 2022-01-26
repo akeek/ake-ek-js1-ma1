89937763c50c9572fa01fc8bc9e4a4170afa46ab
Åke Ek <svein.ek@stud.noroff.no>
2022-01-26 10:55:24 +0100

    create js an html, add answers

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//question 1
+
+const cat = {
+    complain: function(){
+        console.log("Meow!")
+    }
+}
+
+cat.complain();
+
+
+//question 2
+var heading = document.querySelector("h3")
+
+heading.innerHTML = "Updated heading"
+
+
+//question 3
+
+heading.style.fontSize = "2em"
+
+
+//question 4
+//tror ikke at dette er riktig
+var heading = document.querySelector("h3")
+heading.classList.add("Subheading")
