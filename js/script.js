e187606721fa615426818bedfba5e2bcff2b94d9
Åke Ek <svein.ek@stud.noroff.no>
2022-01-28 15:33:09 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 const cat = {
     complain: function(){
-        console.log("Meow!")
+        console.log("Meow!");
     }
 }
 
@@ -12,45 +12,44 @@ cat.complain();
 
 
 //question 2
-const heading = document.querySelector("h3")
+const heading = document.querySelector("h3");
 
-heading.innerHTML = "Updated heading"
+heading.innerHTML = "Updated heading";
 
 
 
 
 //question 3
 
-heading.style.fontSize = "2em"
+heading.style.fontSize = "2em";
 
 
 
 
 //question 4
 
-heading.className = "subheading"
-console.log(heading)
+heading.className = "subheading";
+console.log(heading);
 
 
 
 
 //question 5
 
-const paragraphs = document.querySelectorAll("p")
+const paragraphs = document.querySelectorAll("p");
 
 for(let i = 0; i < paragraphs.length; i++){
-    paragraphs[i].style.color = "red"
+    paragraphs[i].style.color = "red";
 }
 
 
 
 
 //question 6
-var resultsContainer = document.querySelector(".results")
+var resultsContainer = document.querySelector(".results");
 
-resultsContainer.innerHTML = `<p>"New paragraph"</p>`
-resultsContainer.style.backgroundColor = "yellow"
-
+resultsContainer.innerHTML = `<p>"New paragraph"</p>`;
+resultsContainer.style.backgroundColor = "yellow";
 
 
 
@@ -73,31 +72,28 @@ const cats = [
 
 function catFunction(list) {
     for(let i = 0; i < list.length; i++){
-        console.log(list[i].name)
+        console.log(list[i].name);
     }
 }
 
-catFunction(cats)
-
+catFunction(cats);
 
 
 
 
 //question 8
 
-const catContainer = document.querySelector(".cat-container")
-
-
+const catContainer = document.querySelector(".cat-container");
 
 function createCats(cats) {
     for(let i = 0; i < cats.length; i++){
         const catName = cats[i].name;
-        let catAge = cats[i].age;
-        if(catAge === undefined){
-            catAge = ("Age unknown");
-        }
-        catContainer.innerHTML += `<div><h5>${catName}</h5><p>${catAge}</p></div>`
+        let catAge = cats[i].age || "Age unknown";
+        // if(catAge === undefined){
+        //     catAge = ("Age unknown");
+        // }
+        catContainer.innerHTML += `<div><h5>${catName}</h5><p>${catAge}</p></div>`;
     }
 }
 
-createCats(cats)
+createCats(cats);
